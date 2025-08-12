ab9e3ea07ebef3ffae8b814caa11e302af019a03
Aniket Chopade <ani.chopade57@gmail.com>
2025-08-12 13:26:58 -0500

    Update Team.jsx

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -58,6 +58,7 @@ const Team = () => {
                 <TeamCard 
                   key={member.id || index} 
                   {...member} 
+                  role= ""
                   alt={member.full_name}
                 />
               ))}
